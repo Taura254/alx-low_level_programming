d4f696f5458f314bb04002967544511db8d7fc0e
Benjamin Taura <benjaminfondo01@gmail.com>
2021-10-18 03:32:10 -0700

    Backend developer

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,19 +1,18 @@
-#include <stdio.h>
 #include "main.h"
 
-int main(void)
+/**
+ * get_endianness - Checks the endianness.
+ *
+ * Return: If big-endian - 0.
+ *         If little-endian - 1.
+ */
+int get_endianness(void)
 {
-int n;    
-n = get_endianness();
-if (n != 0)
-{
-printf("Little Endian\n");
-}
-else
-{
-printf("Big Endian\n");
- 
-}
-return (0);
- 
+	int num = 1;
+	char *endian = (char *)&num;
+
+	if (*endian == 1)
+		return (1);
+
+	return (0);
 }
